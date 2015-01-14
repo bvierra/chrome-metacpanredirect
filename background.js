2aaaf99de32505ff2ac78ad17c9921b5d4a07895
Billy Vierra <bvierra@sortatechie.com>
2015-01-13 19:42:56 -0800

    Initial commit

---
+++ background.js
@@ -0,0 +1,13 @@
+var host = "http://search.mcpan.org";
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
+    },
+    {
+        urls: [
+            "*://search.cpan.org/*",
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+);
